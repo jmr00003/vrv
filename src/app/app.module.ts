8f7669643ad5719d271c805cd012af37cec7ec3c
Joshua <jmr00003@mix.wvu.edu>
2022-02-01 00:35:13 -0500

    Added Second Card Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TopNavBarComponent } from 'src/app/header/topnavbar.component';
 
 import { AppComponent } from './app.component';
+import { FirstEpisodeCardComponent } from './first-episode-card/first-episode-card.component';
 import { FooterComponent } from './footer/footer.component';
 import { ShowCardComponent } from './show-card/show-card.component';
 
@@ -11,7 +12,8 @@ import { ShowCardComponent } from './show-card/show-card.component';
     AppComponent,
     TopNavBarComponent,
     FooterComponent,
-    ShowCardComponent
+    ShowCardComponent,
+    FirstEpisodeCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/first-episode-card/first-episode-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'vrv-first-ep',
+    templateUrl: 'first-episode-card.component.html',
+    styleUrls: ['first-episode-card.component.css']
+})
+export class FirstEpisodeCardComponent {
+
+}
