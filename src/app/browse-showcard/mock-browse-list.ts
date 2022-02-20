385d177153e9ef9278e41d9b7c7d71cd339894f3
Joshua <jmr00003@mix.wvu.edu>
2022-02-20 18:08:06 -0500

    Finished Styling New Programmatic Component

--- src/app/browse-showcard/mock-browse-list.ts
+++ src/app/browse-showcard/mock-browse-list.ts
@@ -1,75 +1,75 @@
 export const mock_browse_list = [
     {
-        thumbnail:"/assets/show-card/Blank-Poster.png",
-        title:"Black Clover",
+        thumbnail:"/assets/browse-card/Thumbnail1.jpeg",
+        title:"A Centaur's Life",
         type:"Series",
         language:"Sub",
         logo: "/assets/Crunchyroll-Banner.png",
-        route: "/Black-Clover"
+        route: "" 
     },
     {
-        thumbnail:"/assets/show-card/Blank-Poster.png",
-        title:"Black Clover",
+        thumbnail:"/assets/browse-card/Thumbnail2.jpeg",
+        title:"A Certain Magical Index",
         type:"Series",
         language:"Sub",
         logo: "/assets/Crunchyroll-Banner.png",
-        route: "/Black-Clover"
+        route: ""
     },
     {
-        thumbnail:"/assets/show-card/Blank-Poster.png",
-        title:"Black Clover",
+        thumbnail:"/assets/browse-card/Thumbnail3.jpeg",
+        title:"A Certain Scientific Accelerator",
         type:"Series",
         language:"Sub",
         logo: "/assets/Crunchyroll-Banner.png",
-        route: "/Black-Clover"
+        route: ""
     },
     {
-        thumbnail:"/assets/show-card/Blank-Poster.png",
-        title:"Black Clover",
+        thumbnail:"/assets/browse-card/Thumbnail4.jpeg",
+        title:"A Certain Scientific Railgun",
         type:"Series",
         language:"Sub",
         logo: "/assets/Crunchyroll-Banner.png",
-        route: "/Black-Clover"
+        route: ""
     },
     {
-        thumbnail:"/assets/show-card/Blank-Poster.png",
-        title:"Black Clover",
+        thumbnail:"/assets/browse-card/Thumbnail5.jpeg",
+        title:"A Dark Rabbit has Seven Lives",
         type:"Series",
         language:"Sub",
         logo: "/assets/Crunchyroll-Banner.png",
-        route: "/Black-Clover"
+        route: ""
     },
     {
-        thumbnail:"/assets/show-card/Blank-Poster.png",
-        title:"Black Clover",
+        thumbnail:"/assets/browse-card/Thumbnail6.jpeg",
+        title:"A Destructive God Sits Next to Me",
         type:"Series",
         language:"Sub",
         logo: "/assets/Crunchyroll-Banner.png",
-        route: "/Black-Clover"
+        route: ""
     },
     {
-        thumbnail:"/assets/show-card/Blank-Poster.png",
-        title:"Black Clover",
+        thumbnail:"/assets/browse-card/Thumbnail7.jpeg",
+        title:"A Lull in the Sea (Nagi-Asu: Nagi no Asukara)",
         type:"Series",
         language:"Sub",
         logo: "/assets/Crunchyroll-Banner.png",
-        route: "/Black-Clover"
+        route: ""
     },
     {
-        thumbnail:"/assets/show-card/Blank-Poster.png",
-        title:"Black Clover",
+        thumbnail:"/assets/browse-card/Thumbnail8.jpeg",
+        title:"A Place Further Than the Universe",
         type:"Series",
         language:"Sub",
         logo: "/assets/Crunchyroll-Banner.png",
-        route: "/Black-Clover"
+        route: ""
     },
     {
-        thumbnail:"/assets/show-card/Blank-Poster.png",
-        title:"Black Clover",
+        thumbnail:"/assets/browse-card/Thumbnail9.jpeg",
+        title:"A Sister's All You Need.",
         type:"Series",
         language:"Sub",
         logo: "/assets/Crunchyroll-Banner.png",
-        route: "/Black-Clover"
+        route: ""
     },
     
 ];
